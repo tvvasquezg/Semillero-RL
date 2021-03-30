f4a9ce65cfd713d4ac657cd75db7e02253a07872
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2021-03-30 12:42:06 -0500

    Update simple_contextual_bandits.py
    Se hizo compatible con nueva versión de visualizer.py

--- SESION_2/contextual_bandits/simple_contextual_bandits.py
+++ SESION_2/contextual_bandits/simple_contextual_bandits.py
@@ -3,12 +3,16 @@ import matplotlib.pyplot as plt
 from record import Record
 from visualizer import visualize
 
-r = Record(10)
+n_trials = 20
+r = Record(n_trials)
 
 n_states = 2
 K = 5
 
-qs = np.array([[1,0,0,0,0],[0,1,0,0,0]])
+qs = np.array([
+               [1,0,0,0,0],
+               [0,1,0,0,0]
+               ])
 
 Qs = np.zeros((n_states,K))
 Ns = np.zeros((n_states,K))
@@ -29,6 +33,7 @@ def choose_action(state,epsilon=0.1):
             max_idx = np.random.choice(max_idxs)
         else:
             return np.argmax(Qs[state])
+        
         action = max_idx
         
         return action
@@ -43,36 +48,60 @@ def choose_random_state(previous_state):
 timesteps = 10_000
 rewards = np.zeros(timesteps)
 state = 0
-for t in range(100):
+poss = np.zeros(n_trials)
+negs = np.zeros(n_trials)
+for t in range(n_trials):
     state = choose_random_state(state)
     action = choose_action(state)
+    poss[t] = int(state == action)
+    negs[t] = int(state != action)
     #print(state,action)
     r.record_baseline(state,action)
 
 #print('r.base_line[:][1]',r.base_line[:,1])
-visualize(r.base_line[:,0],r.base_line[:,1],10,(255,0,0))
-
+visualize(r.base_line[:,0],r.base_line[:,1],n_trials,(255,0,0),'Before Training',poss,negs)
+input('Press ENTER')
+poss = np.zeros(n_trials)
+negs = np.zeros(n_trials)
 for t in range(timesteps):
     
     state = choose_random_state(state)
     action = choose_action(state)
-    if t>300:
+    
+    if 0<=t<n_trials:
+        poss[t] = int(state == action)
+        negs[t] = int(state != action)
         r.record_while_learning(state,action)
     reward = get_reward(state,action)
     rewards[t] = reward
     Ns[state,action] +=1
    # print('Qs[state,action]',Qs[state,action])
     Qs[state,action] = Qs[state,action] + (1/Ns[state,action])*(reward - Qs[state,action]) 
-visualize(r.learning[:,0],r.learning[:,1],10,(0,0,0))
-
-for t in range(100):
+visualize(r.learning[:,0],r.learning[:,1],n_trials,(0,0,0),'During Training',poss,negs)
+input('Press ENTER')
+poss = np.zeros(n_trials)
+negs = np.zeros(n_trials)
+for t in range(n_trials):
     state = choose_random_state(state)
     action = choose_action(state,epsilon=0)
+    poss[t] = int(state == action)
+    negs[t] = int(state != action)
     r.record_in_test(state,action)
 
-visualize(r.test[:,0],r.test[:,1],10,(0,255,0))
+visualize(r.test[:,0],r.test[:,1],n_trials,(0,255,0),'After training',poss,negs)
+input('Press ENTER')
 plt.plot(np.cumsum(rewards)/(np.arange(timesteps)+1))
 plt.show()
+
+
+
+    
+
+
+
+
+
+
 
 
 
